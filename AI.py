99479f4e267b8e6b0206cbc736acf2ef682bdd02
Kashif Ali Khan <113260067+kashifalikhan36@users.noreply.github.com>
2023-10-26 12:50:21 +0530

    Add AI_assistent and soem new trained data

---
+++ AI.py
@@ -0,0 +1,25 @@
+import openai
+import json
+class Ai_assistent:
+    
+    def __init__(self,API_KEY):
+        openai.api_key = API_KEY
+        data=""
+        with open('data/ai_train.json','r') as file:
+            data = json.load(file)
+        self.messages=data
+    def Assistent_listen(self,discription_image):
+        self.messages.append(
+            {"role": "user", "content": discription_image},
+        )
+
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-16k", messages=self.messages
+        )
+
+        prompt = chat.choices[0].message.content
+        print("Ai Says")
+        self.messages.append({"role": "assistant", "content": prompt})
+        with open('data/ai_train.json',"w") as file:
+            json.dump(self.messages, file)
+        return prompt
---
+++ main.py
@@ -0,0 +1,5 @@
+api_key="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"
+from AI import Ai_assistent
+while True:
+    AI=Ai_assistent(api_key)
+    print(AI.Assistent_listen(input()))
